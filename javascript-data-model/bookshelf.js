7bb16c534adca3272b78d61f70ac192962102f96
Sean-Hughes-1 <coronapilot69@gmail.com>
2022-12-13 19:09:50 +0000

    Sean Hughes - completed javascript-data-model

---
+++ javascript-data-model/bookshelf.js
@@ -0,0 +1,20 @@
+var bookshelf = [
+  {
+    isbn: '978-1491929483',
+    title: 'Introduction to JavaScript Object Notation: A To-the-Point Guide to JSON',
+    author: 'Lindsay Bassett'
+  },
+  {
+    isbn: '978-1484218624',
+    title: 'JSON Quick Syntax Reference',
+    author: 'Wallace Jackson'
+  },
+  {
+    isbn: '978-0692232699',
+    title: 'Build APIs You Won\'t Hate: Everyone and their dog wants an API, so you should probably learn how to build them',
+    author: 'Phil Sturgeon & Laura Bohill'
+  }
+];
+console.log('The author of the second book is: ', bookshelf[1].author);
+console.log('The ISBN of the first book is: ', bookshelf[0].isbn);
+console.log('The title of the third book is: ', bookshelf[2].title);
