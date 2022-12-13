bb83de189b5394310c74b4c1d38469883a1bd281
Sean-Hughes-1 <coronapilot69@gmail.com>
2022-12-13 00:50:01 +0000

    Sean Hughes - completed javascript-arrays excercise.

---
+++ javascript-arrays/main.js
@@ -0,0 +1,17 @@
+var colors = ['red', 'white', 'blue'];
+console.log('value of index[0]: ', colors[0]);
+console.log('value of index[1]: ', colors[1]);
+console.log('value of index[2]: ', colors[2]);
+var america = 'America is ' + colors[0] + ' ' + colors[1] + ' ' + colors[2];
+console.log(america);
+colors[2] = 'green';
+var mexico = 'Mexico is ' + colors[0] + ' ' + colors[1] + ' ' + colors[2];
+console.log(mexico);
+console.log(colors);
+var students = ['John', 'Suzanne', 'Steve', 'Mike'];
+var numberOfStudents = students.length;
+console.log('There are ' + numberOfStudents + ' students in the class.');
+var lastIndex = numberOfStudents - 1;
+var lastStudent = students[lastIndex];
+console.log('The last student in the array is ', lastStudent);
+console.log('value of students: ', students);
