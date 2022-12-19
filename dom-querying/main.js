2526dc0d27896539d2cff2bb8e24151498b2cf2a
Sean-Hughes-1 <coronapilot69@gmail.com>
2022-12-19 18:41:54 +0000

    Sean Hughes - Completed dom-querying

---
+++ dom-querying/main.js
@@ -0,0 +1,9 @@
+console.log('hello, world');
+console.log(document.querySelector('h1'));
+console.dir('h1');
+console.log(document.querySelector('#explanation'));
+console.dir('#explanation');
+console.log(document.querySelector('.hint'));
+console.dir('.hint');
+console.log(document.querySelectorAll('p'));
+console.log(document.querySelectorAll('.example-link'));
