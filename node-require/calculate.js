5fe1e6bdd832f719638a9eb0aecf8a8eddb32cdd
Sean-Hughes-1 <coronapilot69@gmail.com>
2023-01-24 23:20:04 +0000

    Sean completed node-require

---
+++ node-require/calculate.js
@@ -0,0 +1,32 @@
+const add = require('./add');
+const subtract = require('./subtract');
+const multiply = require('./multiply');
+const divide = require('./divide');
+const num1 = Number(process.argv[2]);
+const operator = process.argv[3];
+const num2 = Number(process.argv[4]);
+var result = 0;
+switch (operator) {
+  case 'plus':
+    result = add(num1, num2);
+    console.log('result: ', result);
+    result = 0;
+    break;
+  case 'minus':
+    result = subtract(num1, num2);
+    console.log('result: ', result);
+    result = 0;
+    break;
+  case 'times':
+    result = multiply(num1, num2);
+    console.log('result: ', result);
+    result = 0;
+    break;
+  case 'over':
+    result = divide(num1, num2);
+    console.log('result: ', result);
+    result = 0;
+    break;
+  default:
+    console.log('Invalid Operation. Valid operators: plus, minus, times, over');
+}
