4c7c7a9d0b0cceca1b3bb22c2797970b2bd5e34d
Sean-Hughes-1 <coronapilot69@gmail.com>
2023-01-25 17:35:14 +0000

    Sean completed node-fs-readfile

---
+++ node-fs-readfile/read-any.js
@@ -0,0 +1,9 @@
+const fs = require('fs');
+const file = process.argv[2];
+fs.readFile(file, 'utf8', (err, data) => {
+  if (err) {
+    console.error(err);
+    return;
+  }
+  console.log(data);
+});
