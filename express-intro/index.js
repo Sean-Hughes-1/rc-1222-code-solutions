550297953dd6121b697c9b04d0aaf708297b7375
Sean-Hughes-1 <coronapilot69@gmail.com>
2023-01-26 16:51:22 +0000

    Sean completed express-intro

---
+++ express-intro/index.js
@@ -0,0 +1,6 @@
+const express = require('express');
+const app = express();
+app.listen(3000, () => {
+  // eslint-disable-next-line no-console
+  console.log('Express server listening on port 3000');
+});
