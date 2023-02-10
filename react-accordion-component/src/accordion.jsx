9e19e5964d9641fda8e328a53ea02f715c33e875
Sean-Hughes-1 <coronapilot69@gmail.com>
2023-02-10 23:36:44 +0000

    Refactored to dynamically render each button and section from an array of objects

--- react-accordion-component/src/accordion.jsx
+++ react-accordion-component/src/accordion.jsx
@@ -22,22 +22,18 @@ export class Accordion extends React.Component {
   }
 
   render() {
+    const { data } = this.props;
+
     return (
       <>
-        <button className="accordion" onClick={() => this.handleClick(0)}>Section 1</button>
-        <div className={this.state.openIndex === 0 ? 'panel open' : 'panel'}>
-          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
-        </div>
-
-        <button className="accordion" onClick={() => this.handleClick(1)}>Section 2</button>
-        <div className={this.state.openIndex === 1 ? 'panel open' : 'panel'}>
-          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
-        </div>
-
-        <button className="accordion" onClick={() => this.handleClick(2)}>Section 3</button>
-        <div className={this.state.openIndex === 2 ? 'panel open' : 'panel'}>
-          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
-        </div>
+        {data.map((section, index) => (
+          <>
+            <button className="accordion" onClick={() => this.handleClick(index)}>{section.title}</button>
+            <div className={this.state.openIndex === index ? 'panel open' : 'panel'}>
+              <p>{section.content}</p>
+            </div>
+          </>
+        ))}
       </>
     );
   }
--- react-accordion-component/src/index.jsx
+++ react-accordion-component/src/index.jsx
@@ -4,5 +4,21 @@ import { Accordion } from './accordion';
 
 const container = document.querySelector('#root');
 const root = ReactDOM.createRoot(container);
-
-root.render(<Accordion />);
+const data = [
+  {
+    index: '1',
+    title: 'section 1',
+    content: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.'
+  },
+  {
+    index: '2',
+    title: 'section 2',
+    content: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.'
+  },
+  {
+    index: '3',
+    title: 'section 3',
+    content: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.'
+  }
+];
+root.render(<Accordion data={data}/>);
